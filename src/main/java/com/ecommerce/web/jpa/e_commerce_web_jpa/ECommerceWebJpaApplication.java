5ff8c9fab490620db43b95d173935cb97fdeb3b7
Diamond Patrick <diamondpaatrick@gmail.com>
2026-05-16 15:56:15 +0700

    first commit

---
+++ src/main/java/com/ecommerce/web/jpa/e_commerce_web_jpa/ECommerceWebJpaApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.web.jpa.e_commerce_web_jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceWebJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECommerceWebJpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ecommerce/web/jpa/e_commerce_web_jpa/ECommerceWebJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.web.jpa.e_commerce_web_jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceWebJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
