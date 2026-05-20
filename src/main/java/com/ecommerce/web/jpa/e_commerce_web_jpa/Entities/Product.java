73efe9a407524a5845bd7588d636a8c8c11b4a46
Diamond Patrick <diamondpaatrick@gmail.com>
2026-05-20 15:28:31 +0700

    create product entity

---
+++ src/main/java/com/ecommerce/web/jpa/e_commerce_web_jpa/Entities/Product.java
@@ -0,0 +1,40 @@
+package com.ecommerce.web.jpa.e_commerce_web_jpa.Entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.Lob;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "product")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Product {
+
+    @Id
+    @Column(name = "id_product")
+    private String idProduct;
+
+    @Column(name = "product_name")
+    private String productName;
+
+    private double harga;
+
+    @Lob
+    private byte[] gambar;
+
+    private String description;
+
+    @ManyToOne
+    @JoinColumn(name = "seller_id", referencedColumnName = "id")
+    private Seller seller;
+}
