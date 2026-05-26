0a06b4c5b31065d56bbf99488de036544f9aa1aa
Diamond Patrick <diamondpaatrick@gmail.com>
2026-05-26 14:20:23 +0700

    create ProductCategory and Role enum class for use in Product and Staff class

---
+++ src/main/java/com/ecommerce/web/jpa/e_commerce_web_jpa/Entities/Enum/ProductCategory.java
@@ -0,0 +1,7 @@
+package com.ecommerce.web.jpa.e_commerce_web_jpa.Entities.Enum;
+
+public enum ProductCategory {
+    KAIN,
+    BAJU,
+    AKSESORIS
+}
---
+++ src/main/java/com/ecommerce/web/jpa/e_commerce_web_jpa/Entities/Enum/Role.java
@@ -0,0 +1,8 @@
+package com.ecommerce.web.jpa.e_commerce_web_jpa.Entities.Enum;
+
+public enum Role {
+
+    CEO,
+    FINANCE,
+    STAFF
+}
